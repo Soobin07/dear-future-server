23be6b57c4fa6676f3559218679d667c3a6c42ef
Soobin07 <subin_j92@naver.com>
2026-06-02 23:57:52 +0900

    init Spring Boot project

---
+++ src/main/java/com/dearfuture/DearfutureApplication.java
@@ -0,0 +1,13 @@
+package com.dearfuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DearfutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DearfutureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dearfuture/DearfutureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dearfuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DearfutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
